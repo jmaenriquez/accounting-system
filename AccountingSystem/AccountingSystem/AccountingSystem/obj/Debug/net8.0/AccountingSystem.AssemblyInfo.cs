8ca77523244c9c0fd345ca51df63a9f74f635c7d
Dan <canoza.dannyllojohn@gmail.com>
2025-03-19 10:45:20 +0800

    Side nav and Top nav Responsive

--- AccountingSystem/AccountingSystem/AccountingSystem/obj/Debug/net8.0/AccountingSystem.AssemblyInfo.cs
+++ AccountingSystem/AccountingSystem/AccountingSystem/obj/Debug/net8.0/AccountingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7528db42e3e6a23667879b7e16ce89b2f99ab371")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5208e98c1f4f0fecde05c0df442b88c338d85eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
