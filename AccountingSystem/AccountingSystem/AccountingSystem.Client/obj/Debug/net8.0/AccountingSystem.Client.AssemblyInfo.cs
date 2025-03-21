e3a2f3eb851f874099f3baa4810b9f2603dd0215
John Matthew Enriquez <johnmatthewenriquez1031@gmail.com>
2025-03-21 14:31:03 +0800

    Nav Menu and MainLayout updated

--- AccountingSystem/AccountingSystem/AccountingSystem.Client/obj/Debug/net8.0/AccountingSystem.Client.AssemblyInfo.cs
+++ AccountingSystem/AccountingSystem/AccountingSystem.Client/obj/Debug/net8.0/AccountingSystem.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountingSystem.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96701e5abeb45aafb50a6e3aad552aaaa41aea7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae269b1bb553b16239f2671bb5baa469c08b6d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountingSystem.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountingSystem.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
