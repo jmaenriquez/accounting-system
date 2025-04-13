8bcef7a6cf98092e904db80a2fd4041475975990
John Matthew Enriquez <johnmatthewenriquez1031@gmail.com>
2025-04-13 23:00:25 +0800

    Reports Page Made (Needs improvement)

--- AccountingSystem/AccountingSystem/AccountingSystem/obj/Debug/net8.0/AccountingSystem.AssemblyInfo.cs
+++ AccountingSystem/AccountingSystem/AccountingSystem/obj/Debug/net8.0/AccountingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20d09de753793da7577d5519760b5d60b1b87ca6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae74e787882be7ddba46e67effd7fcdbca5e023")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
